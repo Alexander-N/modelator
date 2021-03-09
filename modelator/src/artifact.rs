76603a44c1c0497e299020407933e1e00d4d4a94
Andrey Kuprianov <andrey@informal.systems>
2021-03-09 12:27:46 +0100

    add artifacts (#16)

--- modelator/src/artifact.rs
+++ modelator/src/artifact.rs
@@ -1,7 +1,16 @@
 use serde::{Deserialize, Serialize};
-use std::fmt;
+use std::{fmt, path::Path, str};
+use crate::error::Error;
 
+pub mod model;
 pub mod trace;
+
+
+pub enum ArtifactFormat {
+    TLA,
+    JSON,
+    TLC
+}
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct ArtifactManifest {
@@ -10,15 +19,19 @@ pub struct ArtifactManifest {
     pub typ: &'static str,
 }
 
-enum Type {
-    TLA,
-}
 
 pub trait Artifact: fmt::Display {
-    //fn name() -> &'static str;
+    fn name(&self) -> &'static str;
+    fn typ(&self) -> &'static str;
+    fn formats(&self) -> Vec<ArtifactFormat>;
+
+    fn from_string(s: &str) -> Result<Self, Error> where Self: Sized;
+    fn from_file(f: &Path) -> Result<Self, Error> where Self: Sized;
+    fn to_file(&self, f: &Path, format: ArtifactFormat) -> Result<(), Error>;
 }
 
-impl fmt::Debug for Artifact {
+impl fmt::Debug for dyn Artifact
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         todo!()
     }
